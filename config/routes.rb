7832a119cd9f56d0f439c8cbe884a606673d2b3a
sgeezy <gmixlpixl@gmail.com>
2017-08-01 15:11:04 +0000

    Install devise, use devise to generate user model with views and routing

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
+  # get 'pages/welcome'
+  root 'pages#welcome'
+
+  devise_for :users
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
